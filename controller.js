f42229931b550ce2e9fc94c52d6354f83135a754
lolsonic1234 <anthonydaluz@hotmail.com>
2016-10-19 22:00:12 -0400

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".bar1").css({
+        top: "0vw",
+    })
+    $(".bar2").css({
+        top: "0vw",
+    })
+    $(".bar3").css({
+        top: "0vw",
+    })
+});
